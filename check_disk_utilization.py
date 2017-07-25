500f79b8911258b649d5dd8731a55d51e84a351e
root <root@gs-syslog.localdomain>
2017-07-25 19:33:04 +0000

    pep8 of check_disk_utilization.py

--- check_disk_utilization.py
+++ check_disk_utilization.py
@@ -19,14 +19,15 @@ class FilesystemUsagePercentage(nagiosplugin.Resource):
     """
     def probe(self):
         _log.info('reading data from df')
-        
-        df_output_lines = [s.split() for s in os.popen("df -Ph").read().splitlines()]        
+
+        df_output_lines = [s.split() for s in os.popen("df -Ph").read().splitlines()]
         _log.debug('df_output_lines %s', df_output_lines)
-        _log.debug('range %s', range(1,len(df_output_lines)))        
-        for i in range(1,len(df_output_lines)):
-            _log.debug('Filesystem %s %s' % (df_output_lines[i][5],df_output_lines[i][4][:-1]) )        
+        _log.debug('range %s', range(1, len(df_output_lines)))
+        for i in range(1, len(df_output_lines)):
+            _log.debug('Filesystem %s %s' % (df_output_lines[i][5], df_output_lines[i][4][:-1]))
             yield nagiosplugin.Metric('Usage %s' % df_output_lines[i][5], float(df_output_lines[i][4][:-1]), min=0,
                                       context='FilesystemUsagePercentage')
+
 
 class FilesystemUsageSummary(nagiosplugin.Summary):
 
@@ -34,16 +35,16 @@ class FilesystemUsageSummary(nagiosplugin.Summary):
         ''' Super-classes summary so we can get verbose results '''
         super(FilesystemUsageSummary, self).verbose(results)
 
-
     def ok(self, results):
         return "[" + "][".join(
-                        "%s = %s (State= %s)" % (x.metric.name,x.metric,x.state) 
+                        "%s = %s (State= %s)" % (x.metric.name, x.metric, x.state)
                         for x in results
                         ) + "]"
 
     def problem(self, results):
-        return self.ok( results)
-    
+        return self.ok(results)
+
+
 @nagiosplugin.guarded
 def main():
     argp = argparse.ArgumentParser(description=__doc__)
@@ -55,11 +56,11 @@ def main():
                       help='increase output verbosity (use up to 3 times)')
     args = argp.parse_args()
     check = nagiosplugin.Check(
-        FilesystemUsagePercentage(),\
-        nagiosplugin.ScalarContext('FilesystemUsagePercentage',args.warning, args.critical),\
+        FilesystemUsagePercentage(),
+        nagiosplugin.ScalarContext('FilesystemUsagePercentage', args.warning, args.critical),
         FilesystemUsageSummary(),
         )
-        
+
     check.main(verbose=args.verbose)
 
 if __name__ == '__main__':
